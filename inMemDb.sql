fbcc677fd21be6e2a8be8d3fc0e8fcf6792c5ec5
Dakota <bazookaharwell@gmail.com>
2017-04-25 09:34:50 -1000

    Create users table

--- inMemDb.sql
+++ inMemDb.sql
@@ -26,4 +26,30 @@ VALUES ('st', 'student');
 INSERT INTO employers (id, fullname)
 VALUES ('na', 'N/A');
 
+--Create type job as enum
+CREATE TYPE JOB AS ENUM ('dv', 'na', 'st');
 
+--Create table users
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  age INT NOT NULL,
+  gender VARCHAR(1) NOT NULL,
+  jobtype JOB DEFAULT 'na'
+  );
+
+--Populate users
+INSERT INTO users (name, age, gender, jobtype) VALUES
+  ('Alice', 12, 'f', 'st'),
+  ('Bob', 21, 'm', 'dv'),
+  ('Claire', 56, 'f', 'dv'),
+  ('David', 9, 'm', 'na'),
+  ('Eric', 61, 'm', 'dv'),
+  ('Fred', 45, 'm', 'dv'),
+  ('George', 38, 'm', 'dv'),
+  ('Hannah', 15, 'f', 'na'),
+  ('Ilona', 17, 'f', 'st'),
+  ('Jake', 27, 'm', 'dv'),
+  ('Kathy', 8, 'f', 'na'),
+  ('Liam', 20, 'm', 'st');
